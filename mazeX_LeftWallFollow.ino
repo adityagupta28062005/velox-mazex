d19e2f730a7318e6aa5cf82d42ba9a0e7e8ac137
adityagupta28062005 <146204238+adityagupta28062005@users.noreply.github.com>
2023-11-01 18:29:12 +0530

    Update mazeX_LeftWallFollow.ino
    Changed pin slots according to how Abhay Gill arranged

--- mazeX_LeftWallFollow.ino
+++ mazeX_LeftWallFollow.ino
@@ -1,11 +1,11 @@
 // Motor A connections
-int enA = 9;
-int in1 = 8;
-int in2 = 7;
+int enA = 3;
+int in1 = 2;
+int in2 = 4;
 // Motor B connections
-int enB = 3;
-int in3 = 5;
-int in4 = 4;
+int enB = 6;
+int in3 = 7;
+int in4 = 8;
 
 int noOfIR 5;
 int motorSpeed 100;
